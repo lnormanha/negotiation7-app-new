7db3aeb077adbfea04981b7f13f94df57631fb68
lnormanha <luiznormanha@gmail.com>
2023-08-06 17:37:33 -0300

    feat: migrate fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import {
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
+
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 
@@ -30,6 +31,11 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
     ...FontAwesome.font,
   });
 
